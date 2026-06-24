82a57cef2621c43a7e0710d815ae7d8bd6165ceb
Prateeks078 <itsprateek078@gmail.com>
2026-06-24 23:57:56 +0530

    Some More work added

---
+++ L2_NestingMemberFunc.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // You can declare the objects along with the classes 
+    // For example:
+    
+    /* class Employee{
+            Class Definition        
+     }Harry, Lovish,Raj; */
+    
+     
+
+    return 0;
+}
