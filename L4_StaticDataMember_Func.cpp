401eee6dad1ec914c73abecbafd794c4ec12d9fb
Prateeks078 <itsprateek078@gmail.com>
2026-07-05 23:05:22 +0530

    Static Method Functions Module Added

--- L4_StaticDataMember_Func.cpp
+++ L4_StaticDataMember_Func.cpp
@@ -16,6 +16,19 @@ class Employee{
     void getData(void){
         cout<<"The id of the Employee is: "<<id<<" and the count of this Employee is: "<<count<<endl;
     }
+
+
+
+
+
+
+    // Static Method Function: Always Remember, static method can only access static data members no others are allowed here 
+    static void  getCount(){
+        // cout<<id<<endl; throws error 
+        cout<<"The value of the count is: "<<count<<endl;
+    }
+
+
 };
 
 int Employee::count=1000; // It gets initialised to Zero Very Very Very Very Important Line 
@@ -24,12 +37,16 @@ int main(){
     Employee e1,e2,e3;
     e1.setData();
     e1.getData();
+    // Because static method is always governed by Class not object. Hence 
+    Employee::getCount();
 
     e2.setData();
     e2.getData();
+    Employee::getCount();
     
     e3.setData();
     e3.getData();
+    Employee::getCount();
 
     return 0;
 }
