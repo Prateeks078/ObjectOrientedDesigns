4f72fc7c363d838e64a43f0e085990e190fc9e0c
Prateeks078 <itsprateek078@gmail.com>
2026-07-05 23:00:15 +0530

    Static Data Member Module is added

---
+++ L4_StaticDataMember_Func.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class Employee{
+    private:
+    int id;
+    static int count; // Must be defined outside the class, it tells the compiler that a variable named as count exists but no memory is allocated yet 
+
+    public:
+    void setData(void){
+        cout<<"Enter the id of the Employee: "<<endl;
+        cin>>id;
+        count++;
+    }
+
+    void getData(void){
+        cout<<"The id of the Employee is: "<<id<<" and the count of this Employee is: "<<count<<endl;
+    }
+};
+
+int Employee::count=1000; // It gets initialised to Zero Very Very Very Very Important Line 
+
+int main(){
+    Employee e1,e2,e3;
+    e1.setData();
+    e1.getData();
+
+    e2.setData();
+    e2.getData();
+    
+    e3.setData();
+    e3.getData();
+
+    return 0;
+}
