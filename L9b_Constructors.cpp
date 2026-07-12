34493fb15e5b620692633c63fea43887d5b10086
Prateeks078 <itsprateek078@gmail.com>
2026-07-12 11:36:24 +0530

    Adding Parameterized Constructor Module

--- L9b_Constructors.cpp
+++ L9b_Constructors.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
 using namespace std;
 
+// Implementing Parameterized Constructors 
+
+class Complex{
+    private:
+    int a,b;
+
+    public:
+    // Writing a Parameterized Constructor
+    Complex(int arg1, int arg2){
+        a=arg1;
+        b=arg2;
+    }
+
+    // Function to print the Complex Number 
+    void PrintNum(void){
+        cout<<"The Complex Number is: "<<a<<" + "<<b<<"i";
+    }
+};
+
 int main(){
+    Complex c1(1,2);
+    c1.PrintNum();
     return 0;
 }
