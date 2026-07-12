d1cf3d4e850ef3c75e9de2a08a105366254a34e7
Prateeks078 <itsprateek078@gmail.com>
2026-07-12 13:32:15 +0530

    Adding an Implementation of Distance Formula with Parameterized Constructor

--- L9c_ConstructorEx.cpp
+++ L9c_ConstructorEx.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
+#include <math.h>
 using namespace std;
+
+// Complete the Task for Calculating the Distance between 2 coordinates 
 
 class Point{
     private:
     int x,y;    
+    friend double CalculateDist(Point,Point);
 
     public:
     // Using Parameterized Constructor Here 
@@ -17,8 +21,27 @@ class Point{
     }
 };
 
+// Using Friend Function
+
+double CalculateDist(Point p1,Point p2){
+    int temp1,temp2;
+    temp2=p2.y-p1.y;
+    temp1=p2.x-p1.x;
+    
+    temp2=temp2*temp2;
+    temp1=temp1*temp1;
+
+    return sqrt(temp2+temp1);
+}
+
 int main(){
-    Point p(1,2);
-    p.GetPoint();
+    Point pt1(1,2);
+    Point pt2(3,4);
+
+    pt1.GetPoint();
+    pt2.GetPoint();
+
+    double ans=CalculateDist(pt1,pt2);
+    cout<<"The Distance Calculated between these 2 Points are: "<<ans<<endl;
     return 0;
 }
