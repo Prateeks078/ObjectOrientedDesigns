35cab23a8e45d29445f9f6de0a1eb7f1151c13f3
prateek9909 <prateek.sharma@humanpoweredhealth.com>
2026-07-16 01:33:19 +0530

    Ambigious in Inheritance Added

--- L16_InheritanceAmbiguity.cpp
+++ L16_InheritanceAmbiguity.cpp
@@ -1,6 +1,34 @@
 #include<iostream>
 using namespace std;
 
+
+class Base1{
+    public:
+    void Greet(){
+        cout<<"Amrita is Greeting !!"<<endl;
+    }
+};
+
+class Base2{
+    public:
+    void Greet(){
+        cout<<"Malika is Greeting !!"<<endl;
+    }
+};
+
+class Derived:public Base1, public Base2{
+
+};
+
 int main(){
+    Base1 B1;
+    B1.Greet();
+
+    Base2 B2;
+    B2.Greet();
+
+    Derived D1;
+    // D1.greet(); --> This is Ambiguity 1 
+
     return 0;
 }
