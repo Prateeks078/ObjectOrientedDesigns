4f09d5277a89258aea66e643559bf9de5e00f7a0
prateek9909 <prateek.sharma@humanpoweredhealth.com>
2026-07-16 11:26:06 +0530

    Added Ambiguity-2

--- L16_InheritanceAmbiguity.cpp
+++ L16_InheritanceAmbiguity.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 using namespace std;
 
-
 class Base1{
     public:
     void Greet(){
@@ -16,8 +15,24 @@ class Base2{
     }
 };
 
-class Derived:public Base1, public Base2{
+// class Derived:public Base1, public Base2{
+// }; --> Telling us Ambiguity-1
 
+// Solution To Ambiguity-1 
+class Derived1:public Base1, public Base2{
+    public:
+    void Greet(){
+        Base1::Greet();
+    }
+};
+
+class Derived2:public Base1,public Base2{ 
+    
+    // Ambiguity 2: If I am Having same function in Base Class, Then priority will be given to the Derived Class 
+    public:
+    void Greet(){
+        cout<<"I am greeting myself, Boys"<<endl;
+    }
 };
 
 int main(){
@@ -27,8 +42,15 @@ int main(){
     Base2 B2;
     B2.Greet();
 
-    Derived D1;
+    Derived1 D1;
     // D1.greet(); --> This is Ambiguity 1 (Derived Class got confused, which Greet() to call on from here !!)
 
+    // After Solution to Amb-1 
+    D1.Greet();
+
+// ----------------------------------------------
+
+    Derived2 D2;
+    D2.Greet(); // ----> Solution to Ambiguity 2
     return 0;
 }
