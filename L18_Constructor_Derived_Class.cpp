0a3a72fc2bc1e218612237055e5349d8212ac2d3
Prateeks078 <itsprateek078@gmail.com>
2026-07-18 12:13:39 +0530

    Playing with Constructor Calling

--- L18_Constructor_Derived_Class.cpp
+++ L18_Constructor_Derived_Class.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 using namespace std;
 
-
 /*
 Order of Execution of Constructors:
 
@@ -27,38 +26,56 @@ Class C:public B, public virtual A{
 
 class Base1{
     protected:
-    int data;
+    int data1;
 
     public:
     Base1(int i){
-        i=data;
+        data1=i;
         cout<<"Base1 Class Constructor is being called.."<<endl;
     }
 
     void PrintBase1(){
-        cout<<"The Value of Data is "<<data<<endl;
+        cout<<"The Value of Data is "<<data1<<endl;
     }
 };
 
 
 class Base2{
     protected:
-    int data;
+    int data2;
 
     public:
     Base2(int i){
-        data=i;
+        data2=i;
+        cout<<"Base2 Class Constructor is being called.."<<endl;
     }
 
     void PrintBase2(){
-        cout<<"The Value of Data is: "<<data<<endl;
+        cout<<"The Value of Data is: "<<data2<<endl;
     }
 };
 
+class Derived:public Base1,public virtual Base2{
+    protected:
+    int derived1;
+    int derived2;
 
+    public:
+    Derived(int a, int b, int c, int d): Base1(a), Base2(b){
+        derived1=c;
+        derived2=d;
 
+        cout<<"The Derived Constructor is being called.. "<<endl;
+    }
+    
+    void PrintDerived(){
+        cout<<"The value of Data is: "<<derived1<<endl;
+        cout<<"The value of Data is: "<<derived2<<endl;
+    }
+};
 
 int main(){
-
+    Derived D1(11,12,13,14);
+    D1.PrintDerived();
     return 0;
 }
