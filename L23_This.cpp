39e9684a23821510a7245b9f58b7b0eeab285931
Prateeks078 <itsprateek078@gmail.com>
2026-07-22 02:14:34 +0530

    Updating the Module

--- L23_This.cpp
+++ L23_This.cpp
@@ -1,6 +1,34 @@
+// ------------------- NUCLEAR IMPT CONCEPT ---------------
+
+
 #include<iostream>
 using namespace std;
 
+//  This Point krta hai uss object ko jisne member function ko call kiya 
+
+class A{
+    private:
+    int a;
+
+    public:
+    // void setData(int a){
+    //     this->a=a;
+    // }
+    
+    // Aliter: We can return *this also na
+    A& setData(int a){
+        this->a=a;
+        return *this;
+    }
+
+    void getData(void){
+        cout<<"The Value of a is: "<<a<<endl;
+    }
+};
+
 int main(){
+    A a;
+    a.setData(999).getData();
+    // a.getData();
     return 0;
 }
